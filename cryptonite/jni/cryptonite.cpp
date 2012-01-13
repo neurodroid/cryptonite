fb489cf072b0c0559e783ceda3c59f44957be4cc
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2012-01-13 03:38:14 +0000

    Added license

--- cryptonite/jni/cryptonite.cpp
+++ cryptonite/jni/cryptonite.cpp
@@ -1,21 +1,18 @@
-/*****************************************************************************
- * Author:   Valient Gough <vgough@pobox.com>
- *
- *****************************************************************************
- * Copyright (c) 2003-2004, Valient Gough
- *
- * This library is free software; you can distribute it and/or modify it under
- * the terms of the GNU General Public License (GPL), as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GPL in the file COPYING for more
- * details.
- *
- */
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+// Copyright (c) 2012, Christoph Schmidt-Hieber
 
 #include <jni.h>
 
@@ -30,8 +27,6 @@ extern "C" {
 JNIEXPORT jint JNICALL
 Java_csh_cryptonite_Cryptonite_encfsBrowse(JNIEnv* env, jobject thiz, jstring srcdir, jstring destdir)
 {
-    // anything that comes from the user should be considered tainted until
-    // we've processed it and only allowed through what we support.
     return 0;
 }
 
