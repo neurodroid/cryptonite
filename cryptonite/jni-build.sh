e25317fdb4fffcddc9dae8b4f8d54ddf744f0a85
Christoph Schmidt-Hieber <christoph.schmidthieber@gmail.com>
2012-02-13 14:18:15 +0000

    Build jni libraries for arm and armv7 in a single go

--- cryptonite/jni-build.sh
+++ cryptonite/jni-build.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
 rm -rf libs/armeabi/* obj/local/armeabi/*
-./cplibs-static.py
-~/android-ndk-r7/ndk-build V=1 APP_STL=gnustl_static APP_ABI=armeabi -j2
 rm -rf libs/armeabi-v7a/* obj/local/armeabi-v7a/*
 ./cplibs-static.py
-~/android-ndk-r7/ndk-build V=1 APP_STL=gnustl_static APP_ABI=armeabi-v7a -j2
+~/android-ndk-r7/ndk-build V=1 APP_STL=gnustl_static APP_ABI="armeabi armeabi-v7a" -j2
