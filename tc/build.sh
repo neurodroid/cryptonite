b719b47fdb955377ebbd15c10773c01a64633bf9
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2012-05-08 22:39:53 +0100

    Update wx and tc build toolchains

--- tc/build.sh
+++ tc/build.sh
@@ -1,1 +1,2 @@
+cd truecrypt-7.1a-source
 make -f ../Makefile.android NOGUI=1 VERBOSE=1 NOTEST=1 WXSTATIC=1
---
+++ tc/patch.sh
@@ -0,0 +1,12 @@
+if [ ! -d truecrypt-7.1a-source ]
+then
+    tar -xjf TrueCrypt\ 7.1a\ Source.tar.gz
+    cd truecrypt-7.1a-source
+    patch -p1 < ../tc-android.patch
+    mkdir -p Pkcs11
+    cd Pkcs11
+    wget ftp://ftp.rsasecurity.com/pub/pkcs/pkcs-11/v2-20/pkcs11.h
+    wget ftp://ftp.rsasecurity.com/pub/pkcs/pkcs-11/v2-20/pkcs11f.h
+    wget ftp://ftp.rsasecurity.com/pub/pkcs/pkcs-11/v2-20/pkcs11t.h
+    cd ..
+fi
