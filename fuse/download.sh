d4578c84f3c1c80446ab856fd0e6c223db31efda
il2am0000 <il2am0000@gmail.com>
2012-07-24 19:16:09 +0100

    Replace off_t with loff_t in FUSE. Addresses issue #18

--- fuse/download.sh
+++ fuse/download.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 git clone https://github.com/seth-hg/fuse-android.git
+find ./fuse-android \( -name "*.c" -o -name "*.h" \) -exec sed -i 's/off_t/loff_t/g' {} \;
+patch -p1 < ./fuse-android.patch
