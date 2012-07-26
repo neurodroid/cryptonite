0b673e943a2d9dd557ca963a55ad9e0a6a706164
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2012-07-26 21:52:36 +0100

    Replace off_t with loff_t in encfs

--- encfs/download.sh
+++ encfs/download.sh
@@ -8,6 +8,7 @@ then
     tar -xzf encfs-1.7.4.tgz
     cd encfs-1.7.4
     patch -p1 < ../encfs-android.patch
+    find ./ \( -name "*.c" -o -name "*.h" -o -name "*.cpp" \) -exec sed -i 's/off_t/loff_t/g' {} \;
     autoreconf --force
     cd ..
 fi
