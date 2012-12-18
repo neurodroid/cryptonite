e0ffe38975737169a76398ddbf0230c64d10c40d
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2012-12-18 20:45:40 +0000

    Use fuse 2.9

--- cryptonite/cplibs-static.py
+++ cryptonite/cplibs-static.py
@@ -42,7 +42,7 @@ for arch in archs:
     else:
         cpfile("../protobuf/protobuf-2.4.1/%s/lib/libprotobuf.a" % arch, target_dir)
         cpfile("../tinyxml/tinyxml/%s/libtinyxml.a" % arch, target_dir)
-    cpfile("../fuse/obj/local/%s/libfuse.a" % arch, target_dir)
+    cpfile("../fuse29/obj/local/%s/libfuse.a" % arch, target_dir)
     cpfile("../rlog/rlog-1.4/%s/lib/librlog.a" % arch, target_dir)
     cpfile("../%s/%s/lib/libencfs.a" % (encfs_dir, arch), target_dir)
     cpfile("../openssl/openssl-%s/%s/libssl.a" % (openssl_version, arch), target_dir)
