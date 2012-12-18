9a8645d65c68c0ccd0390ccc52d5fcded66a34d1
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2012-12-18 20:42:45 +0000

    Ported latest fuse to Android

---
+++ fuse29/build.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+rm -rf libs/armeabi/* obj/local/armeabi/*
+rm -rf libs/armeabi-v7a/* obj/local/armeabi-v7a/*
+~/android-ndk-r8c/ndk-build V=1 APP_STL=gnustl_static APP_ABI="armeabi armeabi-v7a" -j2
