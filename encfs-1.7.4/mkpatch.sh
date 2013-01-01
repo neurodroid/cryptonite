6f2779f41a5ba9a02698b4a8c700ae6e394bde0b
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-01-01 11:20:58 +0000

    Backport r728d0e309fd6 to encfs 1.7.4. Fixes issue #23

--- encfs-1.7.4/mkpatch.sh
+++ encfs-1.7.4/mkpatch.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-diff -x '*.in' -x '*.o' -x '*.lo' -x '*.1' -ur ${HOME}/encfs-1.7.4/encfs ./encfs-1.7.4/encfs > encfs-android.patch
+diff -x '*.in' -x '*.o' -x '*.lo' -x '*.1' -ur ${HOME}/encfs-1.7.4/encfs ./encfs-1.7.4/encfs > encfs-android.patch.new
