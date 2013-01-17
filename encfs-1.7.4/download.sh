f2e51e0bf67bcc2951d9db9a6d332c4de5d2f3b8
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-01-17 17:15:19 +0000

    Make compatible with newer versions of autoconf

--- encfs-1.7.4/download.sh
+++ encfs-1.7.4/download.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
+GSED=`which gsed`
+if [ "${GSED}" = "" ]
+then
+    GSED=`which sed`
+fi
+
 if [ ! -f encfs-1.7.4.tgz ]
 then
     wget http://encfs.googlecode.com/files/encfs-1.7.4.tgz
 fi
-if [ ! -d encfs-1.7.4 ]
-then
-    tar -xzf encfs-1.7.4.tgz
-    cd encfs-1.7.4
-    patch -p1 < ../encfs-android.patch
-    find ./ \( -name "*.c" -o -name "*.h" -o -name "*.cpp" \) -exec sed -i"" 's/off_t/loff_t/g' {} \;
-    autoreconf --force
-    cd ..
-fi
+
+rm -rf encfs-1.7.4
+tar -xzf encfs-1.7.4.tgz
+cd encfs-1.7.4
+patch -p1 < ../encfs-android.patch
+find ./ \( -name "*.c" -o -name "*.h" -o -name "*.cpp" \) -exec ${GSED} -i"" 's/off_t/loff_t/g' {} \;
+autoreconf --force
+cd ..
