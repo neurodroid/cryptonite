464d49e5808821c11d82550bdc1cf9c73399e726
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-09-02 21:46:37 +0100

    Add missing files during autoreconf

--- encfs-1.7.4/download.sh
+++ encfs-1.7.4/download.sh
@@ -15,5 +15,5 @@ tar -xzf encfs-1.7.4.tgz
 cd encfs-1.7.4
 patch -p1 < ../encfs-android.patch
 find ./ \( -name "*.c" -o -name "*.h" -o -name "*.cpp" \) -exec ${GSED} -i"" 's/off_t/loff_t/g' {} \;
-autoreconf --force
+AUTOMAKE="automake --add-missing" autoreconf --force
 cd ..
