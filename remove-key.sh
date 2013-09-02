f0ca44ab151097f6c591c99e518c8a83b288d068
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-09-02 22:16:37 +0100

    Use gsed (if available) to remove keys

--- remove-key.sh
+++ remove-key.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+GSED=`which gsed`
+if [ "${GSED}" = "" ]
+then
+    GSED=`which sed`
+fi
+
 APPKEY1=`cat ./cryptonite/AndroidManifest.xml.key`
 APPKEY2=`cat ./cryptonite/AndroidManifest.xml.key2`
 
-sed -i 's/'$APPKEY1'/db1-/' ./cryptonite/AndroidManifest.xml
-sed -i 's/'$APPKEY2'/db2-/' ./cryptonite/AndroidManifest.xml
+${GSED} -i 's/'$APPKEY1'/db1-/' ./cryptonite/AndroidManifest.xml
+${GSED} -i 's/'$APPKEY2'/db2-/' ./cryptonite/AndroidManifest.xml
