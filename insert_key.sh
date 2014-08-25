43804fedf432f84f55b5cedd5061ac7427477a29
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2014-08-25 21:30:34 +0100

    Use gsed instead of sed if available

--- insert_key.sh
+++ insert_key.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+GSED=`which gsed`
+if [ "${GSED}" = "" ]
+then
+    GSED=`which sed`
+fi
+
 APPKEY1=`cat ./cryptonite/AndroidManifest.xml.key`
 APPKEY2=`cat ./cryptonite/AndroidManifest.xml.key2`
 
-sed -i 's/db1-/'$APPKEY1'/' ./cryptonite/AndroidManifest.xml
-sed -i 's/db2-/'$APPKEY2'/' ./cryptonite/AndroidManifest.xml
+${GSED} -i 's/db1-/'$APPKEY1'/' ./cryptonite/AndroidManifest.xml
+${GSED} -i 's/db2-/'$APPKEY2'/' ./cryptonite/AndroidManifest.xml
