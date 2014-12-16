641ed1222c4ef111b35faeca26e3d2e9f11238fc
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2014-12-16 17:39:20 +0000

    Upgrade ndk ro r10d

--- fuse28/build.sh
+++ fuse28/build.sh
@@ -2,4 +2,4 @@
 
 rm -rf libs/armeabi/* obj/local/armeabi/*
 rm -rf libs/armeabi-v7a/* obj/local/armeabi-v7a/*
-~/android-ndk-r10/ndk-build V=1 APP_STL=gnustl_static APP_ABI="armeabi armeabi-v7a" -j2
+~/android-ndk-r10d/ndk-build V=1 APP_STL=gnustl_static APP_ABI="armeabi armeabi-v7a" -j2
